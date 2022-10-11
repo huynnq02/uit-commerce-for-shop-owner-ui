c9d2a4dc67cadd1fadd125d5abac0dcc39e3c468
Việt Vui Vẻ <80948525+19522515@users.noreply.github.com>
2022-10-11 15:23:24 +0700

    Fix Bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
-import {  BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-
+import Home from "./components/pages/Home/Home";
+import List from "./components/pages/List/List";
+import New from "./components/pages/New/New";
+import Single from "./components/pages/Single/Single";
+import Login from "./components/pages/Login/Login";
+import { productInputs, userInputs } from "./formSource";
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="users" >
+          <Route index element={<List/>}/>
+          <Route path=":userId" element={<Single />} />
+          <Route path="new" element={<New inputs={userInputs} title="Add New User" />} />
+        </Route>
+        <Route path="products">
+          <Route index element={<List/>}/>
+          <Route path=":productId" element={<Single />} />
+          <Route path="new" element={<New inputs={productInputs} title="Add New Product" />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
