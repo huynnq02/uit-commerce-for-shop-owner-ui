1227e1ef6d6d004686f00bd9f55319711bcf6deb
Việt Vui Vẻ <80948525+19522515@users.noreply.github.com>
2022-10-17 22:39:40 +0700

    Delete PrivateRoute.jsx

--- PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useContext } from "react";
-import { Route, Navigate, Routes, useLocation } from "react-router-dom";
-import { UserContext } from "./Context/UserContext/UserContext";
-import {Outlet} from "react-router-dom/dist"
-export default function PrivateRoute({ children, ...rest }) {
-  let { isLogin } = useContext(UserContext);
-  return isLogin ? <Outlet/> : <Navigate to="/login" />;
-}
