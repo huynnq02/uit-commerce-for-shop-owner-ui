7ede2f457df1d66fa26d2cce0d4aed23250c821a
Việt Vui Vẻ <80948525+19522515@users.noreply.github.com>
2022-10-17 23:44:29 +0700

    Fix Bug

--- src/components/pages/ManageOrder/ManageOrder.jsx
+++ src/components/pages/ManageOrder/ManageOrder.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import ManageOrderHeader from '../../molecules/ManageOrderHeader/ManageOrderHeader';
-import TabManagerOrder from '../../molecules/TabManagerOrder/TabManagerOrder';
+import React from "react";
+import ManageOrderHeader from "../../molecules/ManageOrderHeader/ManageOrderHeader";
+import TabManagerOrder from "../../molecules/TabManagerOrder/TabManagerOrder";
+import Sidebar from "../../molecules/Sidebar/Sidebar";
+import Navbar from "../../molecules/Navbar/Navbar";
 const ManageOrder = () => {
-    return (
-        <div className='manageOrder'>
-            <ManageOrderHeader></ManageOrderHeader>
-            <TabManagerOrder></TabManagerOrder>
-        </div>
-    );
-}
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        <ManageOrderHeader></ManageOrderHeader>
+        <TabManagerOrder></TabManagerOrder>
+      </div>
+    </div>
+  );
+};
 
 export default ManageOrder;
