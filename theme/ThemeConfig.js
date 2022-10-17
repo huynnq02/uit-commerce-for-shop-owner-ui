54ec953d88da2c1a4f9ceba7f5fed0de28f35953
Việt Vui Vẻ <80948525+19522515@users.noreply.github.com>
2022-10-17 22:38:46 +0700

    Delete theme directory

--- theme/ThemeConfig.js
+++
@@ -1,7 +0,0 @@
-import { createTheme } from "@mui/material";
-
-export const ThemeConfig = createTheme({
-  typography: {
-    fontFamily: ["Satoshi", "Clash Display", "sans-serif"].join(","),
-  },
-});
