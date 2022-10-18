cb7acea8cf76fba85ce5a1b992277c73285177d3
Việt Vui Vẻ <80948525+19522515@users.noreply.github.com>
2022-10-18 20:27:52 +0700

    Connect Firebase

--- src/components/pages/List/List.jsx
+++ src/components/pages/List/List.jsx
@@ -1,17 +1,34 @@
 import "./List.scss"
 import Sidebar from "../../molecules/Sidebar/Sidebar"
 import Navbar from "../../molecules/Navbar/Navbar"
-import Datatable from "../../organisms/Datatable/Datatable"
+import React, { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../../firebase/firebase-config";
+import DataTable from "../../organisms/DataTable/DataTable";
+
 const List = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const colRef = collection(db, "users");
+      const snapshots = await getDocs(colRef);
+      const docs = snapshots.docs.map((doc) => {
+        const data = doc.data();
+        data.id = doc.id;
+        return data;
+      });
+      setUsers(docs);
+    })();
+  }, []);
   return (
     <div className="list">
-      <Sidebar/>
+      <Sidebar />
       <div className="listContainer">
-        <Navbar/>
-        <Datatable/>
+        <Navbar />
+        <DataTable users={users} />
       </div>
     </div>
-)
-}
+  );
+};
 
-export default List
+export default List;
