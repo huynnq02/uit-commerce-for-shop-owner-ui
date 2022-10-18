80eff6471d11a000ea73efe1bd82facac62b111e
Việt Vui Vẻ <80948525+19522515@users.noreply.github.com>
2022-10-18 20:26:58 +0700

    Add files via upload

--- src/components/organisms/DataTable/DataTable.jsx
+++ src/components/organisms/DataTable/DataTable.jsx
@@ -1,25 +1,17 @@
 import "./DataTable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { Link } from "react-router-dom";
-import { useState } from "react";
 import Box from "@mui/material/Box";
-const Datatable = () => {
-  // Data for tesing
+const DataTable = ({users}) => {
+  // Data for firebase
   const userColumns = [
-    { field: "id", headerName: "ID", headerClassName: "addColor", width: 70 },
+    { field: "id", headerName: "ID", headerClassName: "addColor", width: 150 },
     {
-      field: "user",
+      field: "fullname",
       headerName: "User",
       headerClassName: "addColor",
       width: 250,
-      renderCell: (params) => {
-        return (
-          <div className="cellWithImg">
-            <img className="cellImg" src={params.row.img} alt="avatar" />
-            {params.row.username}
-          </div>
-        );
-      },
+
     },
     {
       field: "email",
@@ -48,91 +40,6 @@ const Datatable = () => {
       },
     },
   ];
-  // Data for tesing
-  const userRows = [
-    {
-      id: 1,
-      username: "VietVuiVe",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      status: "active",
-      email: "vietprovipwe@gmail.com",
-      age: 21,
-    },
-    {
-      id: 2,
-      username: "TuyenPham",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "2snow@gmail.com",
-      status: "passive",
-      age: 42,
-    },
-    {
-      id: 3,
-      username: "KhanhHoangVu",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "3snow@gmail.com",
-      status: "pending",
-      age: 45,
-    },
-    {
-      id: 4,
-      username: "DuyLam",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "4snow@gmail.com",
-      status: "active",
-      age: 16,
-    },
-    {
-      id: 5,
-      username: "VanHung",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "5snow@gmail.com",
-      status: "passive",
-      age: 22,
-    },
-    {
-      id: 6,
-      username: "KhoaLang",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "6snow@gmail.com",
-      status: "active",
-      age: 15,
-    },
-    {
-      id: 7,
-      username: "HaiDang",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "7snow@gmail.com",
-      status: "passive",
-      age: 44,
-    },
-    {
-      id: 8,
-      username: "Frances",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "8snow@gmail.com",
-      status: "active",
-      age: 36,
-    },
-    {
-      id: 9,
-      username: "Roxie",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "snow@gmail.com",
-      status: "pending",
-      age: 65,
-    },
-    {
-      id: 10,
-      username: "Roxie",
-      img: "https://images.pexels.com/photos/1820770/pexels-photo-1820770.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
-      email: "snow@gmail.com",
-      status: "active",
-      age: 65,
-    },
-  ];
-
-  const [data, setData] = useState(userRows);
 
   const actionColumn = [
     {
@@ -143,7 +50,10 @@ const Datatable = () => {
       renderCell: (params) => {
         return (
           <div className="cellAction">
-            <Link to="/users/view" style={{ textDecoration: "none" }}>
+                      <Link
+            to={`/users/${params.row.id}`}
+            style={{ textDecoration: "none" }}
+          >
               <div className="viewButton">VIEW</div>
             </Link>
             <Link to="/users/edit" style={{ textDecoration: "none" }}>
@@ -173,7 +83,7 @@ const Datatable = () => {
 
         <DataGrid
           className="datagrid"
-          rows={data}
+          rows={users}
           columns={userColumns.concat(actionColumn)}
           pageSize={9}
           rowsPerPageOptions={[9]}
@@ -183,4 +93,4 @@ const Datatable = () => {
   );
 };
 
-export default Datatable;
+export default DataTable;
