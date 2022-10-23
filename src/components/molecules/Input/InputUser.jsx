4af0ca27414ee8e9029b67952cfd39f8416618de
Việt Vui Vẻ <80948525+19522515@users.noreply.github.com>
2022-10-23 17:23:38 +0700

    Add Input Admin Component

---
+++ src/components/molecules/Input/InputUser.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useField } from "formik";
+import Label from "../../atoms/Label/Label";
+import "./InputUser.scss";
+const InputUser = ({ type = "text", label = "", icon, ...props }) => {
+  const [field, meta] = useField(props);
+
+  return (
+    <>
+      <Label htmlFor={props.name}>
+        <p className="input__label">{label}</p>
+        <div className="input__container">
+          <input
+            className="input__user"
+            type={type}
+            {...field}
+            {...props}
+          ></input>
+          {icon}
+        </div>
+        {meta.touched && meta.error ? (
+          <p className="input__error">{meta.error}</p>
+        ) : null}
+      </Label>
+    </>
+  );
+};
+
+InputUser.propTypes = {};
+
+export default InputUser;
